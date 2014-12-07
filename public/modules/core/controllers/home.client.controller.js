239141412882b9101b89c2bbfcce091792c36805
Dryvenn <dryvenn@gmail.com>
2014-12-07 17:49:29 +0100

    tried to include ui select

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,33 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
-		// This provides Authentication context.
-		$scope.authentication = Authentication;
-	}
-]);
+var customers = [
+  {
+    name: 'Batman',
+    id: 0
+  },
+  {
+    name: 'James (jim) Gordon',
+    id: 1
+  }
+];
+
+
+angular.module('core')
+.controller('HomeController',
+            function HomeController ($scope, $state, Authentication) {
+	      // This provides Authentication context.
+	      $scope.authentication = Authentication;
+              this.debug = function() {
+              };
+              this.selected = [];
+              this.allCustomers = customers;
+              this.customers = this.allCustomers.map(function(val, ind, arr){return val.name;});
+
+              this.select = function(selected) {
+                $state.go('home.customer', {id:parseInt(selected.id, 10)});
+              };
+
+              $scope.state = $state.current.name;
+
+	    });
