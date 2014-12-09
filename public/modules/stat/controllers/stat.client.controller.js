e2bf9a6ae518696f41131b91e9cded7e66ed6119
Dryvenn <dryvenn@gmail.com>
2014-12-09 12:57:24 +0100

    wip on the pie, see https://github.com/chinmaymk/angular-charts/pull/151/files

--- public/modules/stat/controllers/stat.client.controller.js
+++ public/modules/stat/controllers/stat.client.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('stat', ['customer'])
+angular.module('stat', ['customer', 'angularCharts'])
 .controller('StatController',
             function StatController ($scope, $stateParams, Server, Decode) {
 
--- public/modules/stat/directives/favDrinkNumber.client.directive.js
+++ public/modules/stat/directives/favDrinkNumber.client.directive.js
@@ -1,27 +1,87 @@
 'use strict';
 
-function HistoryController ($scope) {
-  this.formatDate = function(time) {
-    var d = new Date(parseInt(time, 10));
-    var day = d.getDate().toString();
-    var month = (d.getMonth() + 1).toString();
-    day = day.length === 1 ? '0' + day : day;
-    month = month.length === 1 ? '0' + month : month;
-    var year = d.getFullYear().toString();
-    var date_time = d.getHours().toString() + ':' + d.getMinutes().toString() + ':' + d.getSeconds().toString();
-    return day + '/' + month + '/' + year + ' ' + date_time;
-  };
-  
-}
-
 angular.module('stat')
 .directive('favDrinkNumber', function() {
   return {
-    templateUrl: 'modules/stat/directives/drink-history.html',
+    templateUrl: 'modules/stat/directives/fav-drink-number.html',
     scope: {
       data: '=data'
     },
-    controllerAs: 'fdn'
-    //controller: HistoryController
+    controllerAs: 'fdn',
+    controller: function($scope, $timeout) {
+
+      this.who = 'world';
+
+      $timeout(function(){
+        $scope.chartActive = true;
+        
+        $scope.dconfig = {
+          title: 'Products',
+          tooltips: true,
+          labels: false,
+          mouseover: function() {},
+          mouseout: function() {},
+          click: function() {},
+          legend: {
+            display: true,
+            //could be 'left, right'
+            position: 'right'
+          }
+        };
+
+        $scope.ddata = {
+          series: ['Sales', 'Income', 'Expense', 'Laptops', 'Keyboards'],
+          data: [{
+            x: 'Laptops',
+            y: [100, 500, 0],
+            tooltip: 'this is tooltip'
+          }, {
+            x: 'Desktops',
+            y: [300, 100, 100]
+          }, {
+            x: 'Mobiles',
+            y: [351]
+          }, {
+            x: 'Tablets',
+            y: [54, 0, 879]
+          }]
+        };
+
+      }, 1000);
+      this.config = {
+        title: 'Chart of the most purchased items',
+        tooltips: true,
+        labels: true,
+        legend: {
+          display: true,
+          position: 'right'
+        },
+        innerRadius: 0 // can be a percentage like '50%'
+      };
+
+      this.data = {
+        series: ['Purchases'],
+        data: []
+      };
+
+      //we oughta calculate the quantity of every purchase
+      var quantities = {};
+      $scope.data.forEach(function(val, ind, arr) {
+        if(quantities[val.name]) {
+          quantities[val.name] += val.quantity;
+        }
+        else {
+          quantities[val.name] = val.quantity;
+        }
+      });
+      //then we pass it as data
+      for(var quantity in quantities) {
+        this.data.data = {
+          x: quantity,
+          y: quantities[quantity]
+        };
+      }
+
+    } // \controller
   };
 });
